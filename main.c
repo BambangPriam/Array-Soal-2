8323b318fa9dcc0103aecabae55cfc5515d18ac5
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-05 11:58:05 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void tukar(int nilai[5]){
+int t= nilai[0];
+nilai[0]=nilai[4];
+nilai[4]=t;
+
+int t1=nilai[1];
+nilai[1]=nilai[3];
+nilai[3]=t1;
+
+}
+
+void main(){
+int nilai[5]={5,10,6,0,4}, i;
+
+printf("Array A : \n");
+for(i=0;i<5;i++){
+    printf("Elemen ke-%i : %d \n", i, nilai[i]);
+}
+printf("\n");
+
+printf("Array B : \n");
+tukar(nilai);
+for(i=0;i<5;i++){
+    printf("Elemen ke-%i : %d \n", i, nilai[i]);
+}
+}
